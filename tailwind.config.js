3e400593b29e15f67582a3b5491affceb8051297
unknown <alamgirhossaindrubo@gmail.com>
2025-03-11 02:19:01 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          brandPink: "#db1568", // Custom pink color
+        },
+      },
+    },
+    plugins: [],
+  };
+  
